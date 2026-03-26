- Refactoring Helmet: Structure of metadata dictionary (#73) - Fix bug in LongContextGradientModel: scale_factor must be used in backward as well - Token-averaged loss: Correct treatment with multiple devices - SFTDataset: Allow targets to be a list

keys_values/data/sft_dataset.py
@@ -38,6 +38,10 @@ class SFTDataset(LongContextDataset):
     included only if it is given or available from what is done anyway.
     Avoids extra costs due to tokenization.
 
+    It is admissible for `data[idx]["output"]` to be a list of strings.
+    In this case, we choose one of them at random in each
+    :meth:`__getitem__` call. The semantics is that any of the entries
+    is a valid target sequence.
     """
 
     def __init__(
@@ -49,6 +53,7 @@ class SFTDataset(LongContextDataset):
         mask_prompt: bool = True,
         ignore_index: int = -100,
         transform: Optional[Callable[[Dict[str, str]], Dict[str, str]]] = None,
+        seed: Optional[int] = None,
     ) -> None:
         super().__init__(
             data,
@@ -59,6 +64,10 @@ class SFTDataset(LongContextDataset):
         )
         self.mask_prompt = mask_prompt
         self.ignore_index = ignore_index
+        # We need
+        if seed is None:
+            seed = 31415927
+        self._prng = torch.Generator().manual_seed(seed)
 
     def __getitem__(self, idx: int) -> Dict[str, Any]:
         example = self.data[idx]
@@ -71,8 +80,13 @@ class SFTDataset(LongContextDataset):
             prompt,
             max_length=self.max_seq_length,
         )
+        targets = example["output"]
+        if isinstance(targets, list):
+            targets = targets[
+                torch.randint(0, len(targets), (1,), generator=self._prng).item()
+            ]
         encoded_response = self.tokenizer.encode(
-            example["output"],
+            targets,
             bos=False,
             eos=True,
             max_length=self.max_seq_length,

keys_values/head_model.py
@@ -82,6 +82,14 @@ class HeadModel(torch.nn.Module):
         For some loss functions, the loss parts are simply summed up, in which
         case this method returns `None` for all chunks.
 
+        For loss functions which are token-averaged (the default), this
+        returns the number of non-masked target tokens for this chunk. For a
+        chunk not containing target entries (i.e., a prompt chunk), this is 0.
+        The sum of these values across all chunks gives the number of
+        non-masked target tokens for the whole batch. When using gradient
+        averaging over several micro-batches, the reduction must be weighted
+        using these sums.
+
         """
         raise NotImplementedError()
 
@@ -157,7 +165,6 @@ class CrossEntropyOnLogits(HeadModel):
     function. For example, teaching a model to use tools, we can use data cases
     coming from trajectories with several tool calls. Apart from the initial
     prompt, we also mask out the tool outputs in `targets`.
-
     """
 
     NAME = "next_token_prediction"