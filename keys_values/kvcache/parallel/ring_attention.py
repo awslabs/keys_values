01c135c422506c3b39e1d6be44a05b3d05904ae2
Matthias Seeger <matthis@amazon.de>
2026-06-14 11:16:29 +0200

    Debug

--- keys_values/kvcache/parallel/ring_attention.py
+++ keys_values/kvcache/parallel/ring_attention.py
@@ -220,6 +220,7 @@ class RingAttentionDriver:
             self._accum_output = new_output
             self._accum_lse = new_lse
         else:
+            print(f"_accum_lse: ({self._accum_lse.dtype}, {self._accum_lse.shape}); new_lse: ({new_lse.dtype}; {new_lse.shape})")  # DEBUG
             new_accum_lse = torch.maximum(self._accum_lse, new_lse) + torch.log1p(
                 -torch.abs(self._accum_lse - new_lse)
             )
