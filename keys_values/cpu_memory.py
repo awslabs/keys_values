b4fac2f86d41fd818eb3fc00887676292af555cf
Matthias Seeger <matthis@amazon.de>
2026-09-13 21:01:45 +0200

    Make storage directory unique. Do not store list of file names

--- keys_values/cpu_memory.py
+++ keys_values/cpu_memory.py
@@ -12,11 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import atexit
+from datetime import datetime
+from filelock import FileLock, Timeout
 import math
 import os
+from pathlib import Path
 import signal
 import threading
-from typing import Optional, Tuple, List
+from typing import Optional, Tuple
 
 import psutil
 import torch
@@ -33,14 +36,41 @@ class FileBasedExtraMemoryManager:
     _HEADROOM = 1.1  # require 10% more available memory than requested
 
     def __init__(self, tmp_dir: str):
-        # TODO: Need to create something unique from `tmp_dir`, which does not
-        # already exist. This must work concurrently
-        self.tmp_dir = tmp_dir
-        self._files: List[str] = []
+        # Create unique subdirectory
+        self.tmp_dir = self._unique_dir(tmp_dir)
+        self.num_files = 0
         self._lock = threading.Lock()
-        os.makedirs(tmp_dir, exist_ok=True)
+        # Ensure that when program ends, or when it is terminated,
+        # :meth:`cleanup` is called, which removes the files.
         atexit.register(self.cleanup)
         self._install_sigterm_handler()
+
+    def _unique_dir(self, tmp_dir: str) -> str:
+        time_format = "%Y%m%d_%H%M%S"
+        time_stamp = datetime.now().strftime(time_format)
+        tmp_path = Path(tmp_dir)
+        res_dir = None
+        runn_no = -1
+        while res_dir is None:
+            runn_no += 1
+            cand_path = tmp_path / (time_stamp + f"_{runn_no}")
+            if not cand_path.exists():
+                lock_path = cand_path.with_suffix(".lock")
+                lock = FileLock(lock_path, timeout=1)
+                try:
+                    with lock.acquire(timeout=1):
+                        cand_path.mkdir(parents=True, exist_ok=False)
+                        res_dir = str(cand_path)
+                except Timeout:
+                    pass
+                finally:
+                    lock.release()
+                    if lock_path.exists():
+                        lock_path.unlink()
+        return res_dir
+
+    def _filename(self, num: int) -> str:
+        return os.path.join(self.tmp_dir, f"buf_{num}.bin")
 
     def allocate(
         self, shape: Tuple[int, ...], dtype: torch.dtype = torch.float32
@@ -53,11 +83,15 @@ class FileBasedExtraMemoryManager:
         return self._alloc_from_file(shape, dtype, n_bytes)
 
     def cleanup(self) -> None:
+        """
+        Removes all files. The directory itself is not removed.
+
+        """
         with self._lock:
-            files, self._files = self._files, []
-        for path in files:
+            num_files, self.num_files = self.num_files, 0
+        for num in range(num_files):
             try:
-                os.remove(path)
+                os.remove(self._filename(num))
             except FileNotFoundError:
                 pass
 
@@ -65,10 +99,10 @@ class FileBasedExtraMemoryManager:
         self, shape: Tuple[int, ...], dtype: torch.dtype, n_bytes: int
     ) -> torch.Tensor:
         with self._lock:
-            path = os.path.join(self.tmp_dir, f"buf_{len(self._files)}.bin")
-            self._files.append(path)
+            path = self._filename(self.num_files)
+            self.num_files += 1
         storage = torch.UntypedStorage.from_file(path, shared=True, nbytes=n_bytes)
-        if len(self._files) == 1:
+        if self.num_files == 1:
             print(f"Starting to write virtual memory to {self.tmp_dir}")
         return torch.empty(0, dtype=dtype).set_(storage).reshape(shape)
 
