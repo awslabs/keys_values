483bfbfe380f8912d7fc02615bf32e2dda5a3517
Matthias Seeger <matthis@amazon.de>
2026-09-24 08:21:06 +0200

    Small print

--- keys_values/finetune/longcontext_full.py
+++ keys_values/finetune/longcontext_full.py
@@ -434,6 +434,8 @@ def setup_internal(
         raise ValueError(
             "use_sample_metric=True currently supported only for Helmet datasets"
         )
+    if isinstance(data, Helmet) and data.use_old_metrics:
+        print("Using old metrics setup with Helmet: sub_exact_match for QA datasets (this is deprecated!)")
     if head_model is None:
         head_model = default_head_model(data)
     elif head_model not in SUPPORTED_HEAD_MODELS:
